2a1368f6905e6755b943a4828af93484dd7055c0
ntq25012002 <trungquan123cn@gmail.com>
2022-09-05 08:17:07 +0700

    test conflict

---
+++ .history/index_20220905081629.php
@@ -0,0 +1,9 @@
+<?php
+    echo "quanntph18231";
+    echo "quanntph18231";
+    echo "quanntph18231";
+
+    echo " Test thêm 1 dòng vào git hub";
+    
+    echo "Test pull"
+?>
--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     echo "quanntph18231";
     echo "quanntph18231";
 
-    echo " Test thêm 1 dòng vào git hub"
+    echo " Test thêm 1 dòng vào git hub";
     
-    echo "Test pull"
+    echo "Test pull";
+    echo "Test pull";
+    echo "Test pull";
 ?>
