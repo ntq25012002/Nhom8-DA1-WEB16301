970650cd6ff6e25b447537e7e55d98c0a7cad1fd
Nguyễn Trung Quân <93777982+ntq25012002@users.noreply.github.com>
2022-09-05 08:18:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
     echo "quanntph18231";
-    echo "quanntph18231";
-    echo "quanntph18231";
 
     echo " Test thêm 1 dòng vào git hub";
     
-    echo "Test pull";
     echo "Test pull";
     echo "Test pull";
 ?>
